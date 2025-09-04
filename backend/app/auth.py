047a5d2f49d003864421ca32d4e0d0d432d3fb43
Lxrenz14 <ritschlorenz14@gmail.com>
2025-09-04 14:33:20 +0000

    get current user works now

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -8,6 +8,8 @@ from jose import jwt, JWTError, ExpiredSignatureError
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
 from database import get_db
+from models import user_model
+from sqlalchemy import select
 
 load_dotenv()
 
@@ -25,23 +27,27 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(id: int):
+def create_access_token(user_id: int):
     expire = datetime.utcnow() + timedelta(minutes=int(TOKEN_EXPIRE_MINUTES))
-    data = {"sub": id, "exp": expire}
+    data = {"sub": str(user_id), "exp": expire}
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db)):
     error = HTTPException(status_code=401, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except ExpiredSignatureError:
-        raise HTTPException(staus_code=401, detail="Token expired", headers={"WWW-Authenticate": "Bearer"},)
+        raise HTTPException(status_code=401, detail="Token expired", headers={"WWW-Authenticate": "Bearer"},)
     except JWTError:
         raise error
     
-    sub = payload.get("sub")
-    if sub is None:
+    user_id = payload.get("sub")
+    if user_id is None:
         raise error
-    
-    
+    user_id = int(user_id)
+    result = await session.execute(select(user_model.User).where(user_model.User.id == user_id))
+    user = result.scalars().first()
+    if user is None:
+        raise error
+    return user
