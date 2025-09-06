ed74fe92f4f69a96fd4e826f2eb8d1aaa8b678ba
Lxrenz14 <ritschlorenz14@gmail.com>
2025-09-06 06:24:51 +0000

    login im frontend geht theorethisch

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from routers import user_router, auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Password Manager", root_path="/api")
 
 items = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Item(BaseModel):
     text: str #if no default value then it is required
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,5 +1,22 @@
 <script>
+    let email = "";
+    let password = "";
 
+async function Login(){
+    const response = await fetch('http://127.0.0.1:8000/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'accept': "application/json"
+        },
+        body: JSON.stringify({ email, password })
+    });
+    if (!response.ok){
+        throw new Error("Failed")
+    }
+    console.log(response);
+    return response.json();
+}
 
 </script>
 
@@ -7,6 +24,19 @@
   
 </main>
 
+
+<form class="login-form" on:submit|preventDefault={Login}>
+<label for="username">Username</label>
+<p><input id="username" name="username" type="text" placeholder="Your Username.." bind:value={email}/></p> 
+ <label for="password" >Password</label>
+ <input type="text" name="password" id="password" placeholder="Your Password.." bind:value={password}/>
+ <input type="submit" value="Submit"/>
+</form>
+
 <style>
 
+.login-form{
+    padding: 50px;
+    background-color: green;
+}
 </style>
