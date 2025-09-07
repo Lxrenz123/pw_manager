3a20dd380c5829e44af0439fdfe1d11b545f870c
Lxrenz14 <ritschlorenz14@gmail.com>
2025-09-07 11:47:08 +0000

    added post request for vault

--- backend/app/routers/vault_router.py
+++ backend/app/routers/vault_router.py
@@ -18,8 +18,23 @@ async def get_vaults(session: PgAsyncSession, current_user: user_model.User = De
     stmt = select(vault_model.Vault).where(vault_model.Vault.owner_id == current_user.id)
     result = await session.execute(stmt)
     vaults = result.scalars().all()
-
+    
     return vaults
-    
-    
 
+
+
+    
+@router.post("/")
+async def create_vault(session: PgAsyncSession, vault_data: vault_schema.CreateVault, user_id: user_model.User = Depends(get_current_user)):
+     
+    vault = vault_model.Vault(
+    owner_id = user_id,
+    name = vault_data.vaultname
+    )
+    session.add(vault)
+    await session.commit()
+    await session.refresh(vault)
+
+    return vault
+
+    
--- backend/app/schemas/vault_schema.py
+++ backend/app/schemas/vault_schema.py
@@ -2,12 +2,10 @@ from pydantic import BaseModel
 from datetime import datetime, timezone
 
 class CreateVault(BaseModel):
-    owner_id: int
-    name: str
-    created_at: datetime
-    updated_at: datetime
+    vaultname: str
 
 class GetVault(BaseModel):
     name: str
     created_at: datetime
     updated_at: datetime
+
